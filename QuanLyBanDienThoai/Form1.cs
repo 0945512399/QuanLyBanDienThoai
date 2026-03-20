47521a1693fd40d746cd8afd7f99f358c31e86f0
0945512399 <tranductho83@gmail.com>
2026-03-20 15:02:25 +0700

    Code chuc nang Dang nhap C#

--- QuanLyBanDienThoai/Form1.cs
+++ QuanLyBanDienThoai/Form1.cs
@@ -1,9 +1,36 @@
-namespace QuanLyBanDienThoai;
+using System;
+using System.Data;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace QuanLyBanDienThoai
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        KetNoi kn = new KetNoi();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Sự kiện khi nhấn nút Đăng nhập
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string user = txtUsername.Text;
+            string pass = txtPassword.Text;
+
+            string sql = "SELECT * FROM NguoiDung WHERE TenDangNhap = '" + user + "' AND MatKhau = '" + pass + "'";
+            DataTable dt = kn.LayDuLieu(sql);
+
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Đăng nhập thành công! Chào mừng " + dt.Rows[0]["HoTen"].ToString());
+                // Chuyển sang Form chính của đồ án tại đây
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu!");
+            }
+        }
     }
 }
