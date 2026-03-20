90403d6f04a97a7d5d37d540467576eef8d04e51
0945512399 <tranductho83@gmail.com>
2026-03-20 15:26:19 +0700

    Them chuc nang Them moi dien thoai vao database

--- QuanLyBanDienThoai/Form1.cs
+++ QuanLyBanDienThoai/Form1.cs
@@ -20,30 +20,36 @@ namespace QuanLyBanDienThoai
             dgvDienThoai.DataSource = kn.LayDuLieu(sql);
         }
 
-        // Sự kiện Tìm kiếm nhanh khi gõ chữ vào ô txtSearch
+        // Chức năng Thêm mới Điện thoại
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string ma = txtMaDT.Text;
+            string ten = txtTenDT.Text;
+            string gia = txtGiaBan.Text;
+            string sl = txtSoLuong.Text;
+            string hang = txtHangSX.Text;
+
+            // Câu lệnh SQL Thêm dữ liệu
+            string sql = string.Format("INSERT INTO DienThoai VALUES ('{0}', N'{1}', {2}, {3}, N'{4}')", 
+                                        ma, ten, gia, sl, hang);
+
+            if (kn.ThucThi(sql))
+            {
+                MessageBox.Show("Thêm điện thoại thành công!");
+                LoadData(); // Tải lại bảng để thấy dữ liệu mới
+            }
+            else
+            {
+                MessageBox.Show("Lỗi: Kiểm tra lại mã trùng hoặc định dạng số!");
+            }
+        }
+
         private void txtSearch_TextChanged(object sender, EventArgs e)
         {
             string keyword = txtSearch.Text;
-            // Tìm kiếm theo Tên máy hoặc Hãng sản xuất
             string sql = "SELECT MaDT AS [Mã], TenDT AS [Tên Máy], GiaBan AS [Giá], SoLuong AS [Kho], HangSX AS [Hãng] " +
                          "FROM DienThoai WHERE TenDT LIKE N'%" + keyword + "%' OR HangSX LIKE N'%" + keyword + "%'";
-            
             dgvDienThoai.DataSource = kn.LayDuLieu(sql);
-        }
-
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            string user = txtUsername.Text;
-            string pass = txtPassword.Text;
-            string sql = "SELECT * FROM NguoiDung WHERE TenDangNhap = '" + user + "' AND MatKhau = '" + pass + "'";
-            DataTable dt = kn.LayDuLieu(sql);
-
-            if (dt.Rows.Count > 0)
-            {
-                MessageBox.Show("Đăng nhập thành công!");
-                LoadData();
-            }
-            else { MessageBox.Show("Sai tài khoản!"); }
         }
     }
 }
